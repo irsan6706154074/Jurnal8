b83bbb2a4f5d845825f66c3be242e67629e65544
Irsan M Ramdan <ragondt1@gmail.com>
2017-04-07 09:40:48 +0700

    Add files via upload

---
+++ no1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+	import java.util.Scanner;
+public class no1 {
+	
+    public static void main(String[]args) {
+        Scanner gampang = new Scanner(System.in);
+        System.out.print("Masukan Nilai : ");
+        int masukan= gampang.nextInt();
+        long fib[] = new long[masukan];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(int i = 2; i < masukan; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < masukan; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+ 
+}
