05e6bf93e7ae4d5fba9fb01c7c6c2d853ab783b7
Irsan M Ramdan <ragondt1@gmail.com>
2017-04-07 09:51:16 +0700

    Update no1.java

--- no1.java
+++ no1.java
@@ -7,17 +7,19 @@ public class no1 {
         System.out.print("Masukan Nilai : ");
         int masukan= gampang.nextInt();
         long fib[] = new long[masukan];
+	    int jumlah;
          
         fib[0] = 0;
         fib[1] = 1;
          
         for(int i = 2; i < masukan; i++) {
             fib[i] = fib[i-1] + fib[i-2];
+		jumlah+=fib[i];
         }
          
-        for (int i = 0; i < masukan; i++) {
-            System.out.print(fib[i] +  " ");
-        }
+   
+            System.out.print(jumlah +  " ");
+
     }
  
 }
