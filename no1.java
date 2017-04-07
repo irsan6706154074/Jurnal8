25deed5e5cad38998819aa5e043bcebc81b219b0
Irsan M Ramdan <ragondt1@gmail.com>
2017-04-07 09:56:18 +0700

    Update no1.java

--- no1.java
+++ no1.java
@@ -2,24 +2,22 @@ import java.io.*;
 	import java.util.Scanner;
 public class no1 {
 	
-    public static void main(String[]args) {
+       public static void main(String[]args) {
         Scanner gampang = new Scanner(System.in);
         System.out.print("Masukan Nilai : ");
         int masukan= gampang.nextInt();
         long fib[] = new long[masukan];
-	    int jumlah;
          
         fib[0] = 0;
         fib[1] = 1;
          
         for(int i = 2; i < masukan; i++) {
             fib[i] = fib[i-1] + fib[i-2];
-		jumlah+=fib[i];
         }
          
-   
-            System.out.print(jumlah +  " ");
-
+        for (int i = 0; i < masukan; i++) {
+            System.out.print(fib[i] +  " ");
+        }
     }
  
 }
